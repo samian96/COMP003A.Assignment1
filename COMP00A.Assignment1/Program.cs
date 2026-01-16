f2a62e843a38455c44098f9687d987ce3486d667
samian <sam.iannazzo@yahoo.com>
2026-01-15 21:50:48 -0800

    added lines 18-27

--- COMP00A.Assignment1/Program.cs
+++ COMP00A.Assignment1/Program.cs
@@ -15,6 +15,17 @@
             Console.WriteLine("8. The other band is called Shotguns Hold Shells");
             Console.WriteLine("9. I am the bass playaer in these band projects");
             Console.WriteLine("10. Playing bass is a hobby of mine");
+            Console.WriteLine("11. Birds are my favorite pet");
+            Console.WriteLine("12. I have 3 pet birds");
+            Console.WriteLine("13. one parakeet named Captain");
+            Console.WriteLine("14. One parakeet named Beaky");
+            Console.WriteLine("15. One cockatiel named Uno");
+            Console.WriteLine("16. They are allowed out of their cage daily");
+            Console.WriteLine("17. My parakeets are hand trained");
+            Console.WriteLine("18. Birds are high maintanence pets");
+            Console.WriteLine("19. They require a lot of activities");
+            Console.WriteLine("20. Their favorite fruit is apples");
+
         }
     }
 }
